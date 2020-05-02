7c7893ce688c444b6ea76d62eeec1f74e13996f9
Jay Jac <jy_jac@msn.com>
2020-05-02 09:12:16 -0400

    Initial Commit

---
+++ WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by Jay Jac on 5/2/20.
+//  Copyright © 2020 Jacaria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
